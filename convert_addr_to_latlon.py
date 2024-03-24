dccebde31a66a22afa3efc282c28f284edae377a
nat1994 <nathanaelchan94@gmail.com>
2024-03-25 00:07:58 +0800

    Bug fix and cleanup
    - Checks for CSV mode first, then checks for either address or filepath
    - Script is now able to take in either an address or a csv file containing address to produce the lat lon of the addresses
    - Removal of the postal code validation using re.match()
    - Added checks to prevent a crash when an invalid address is given

--- convert_addr_to_latlon.py
+++ convert_addr_to_latlon.py
@@ -9,24 +9,15 @@ import os
 url1 = "https://www.onemap.gov.sg/api/common/elastic/search?searchVal="
 url2 = "&returnGeom=Y&getAddrDetails=Y"
 
-#Regex for exactly 6 digits
-pattern = r'^\d{6}$'
-
-#Checks that the postal code is 6 digit code 
-def validate_addr(addr: str) -> bool:
-    #convert string to integer 
-    return bool(re.match(pattern, addr))
-
-
-#This function takes in a postal code and returns the lat and lon
+#This function takes in an address (postal code OR block and street number) and returns the lat and lon
 def convert_addr(addr: str):
-    print(f"Processing the postal code now...")
+    print(f"Processing the address now...")
     response = requests.get(addr)
 
-    if response.status_code == 200:
-        # print(f"result: {response.text}")
-
-        #Convert to json
+    #Gets the number of found results
+    found = json.loads(response.text)["found"]
+    
+    if found != 0:
         r = json.loads(response.text)['results'][0]
 
         lat = r["LATITUDE"]
@@ -36,7 +27,7 @@ def convert_addr(addr: str):
         return lat, lon
 
     else:
-        print(f"Received 400 response from ONEMAP api :(")
+        print(f"The address provided is not valid. ")
         return None, None
 
 
@@ -81,26 +72,23 @@ def process_csv(filePath: str):
 parser = argparse.ArgumentParser(
                 prog='convert_addr_to_latlon',
                 description='this program converts a postal code to decimal lat long coodinates')
-parser.add_argument('addr', type=str, action='store', help='postal code of the address')
 parser.add_argument('--csv', action='store_true')
+parser.add_argument('--addr', type=str, action='store', help='postal code of the address')
 parser.add_argument('--input', type=str, action='store', help='input file path of the csv with block names')
-
-
 args = parser.parse_args()
 
-addr = args.addr
 csvMode = args.csv
-print(f"Received the address postal code: {addr}")
-print(f"Received the csv flag: {csvMode}")
+print(f"CSV mode: {csvMode}")
 
-if not csvMode:
-    #Check that there are exactly 6 digits in the postal code 
-    # if validate_addr(addr):
-    #     #get the address of the postal code 
-    #     convert_addr(url1+addr+url2)
-    # else:
-    #     print(f"The postal code entered is not valid. Try again with a 6 digit number")
-    convert_addr(url1+addr+url2)
+
+if not csvMode: # Read in an address directly into the api
+    addr = args.addr
+    print(f"Received the address postal code: {addr}")
+
+    if addr == None:
+        print(f"Please input a value for addr!")
+    else:
+        convert_addr(url1+addr+url2)
 
 else: #process a csv of postal codes
     csvFilePath = args.input
@@ -110,3 +98,5 @@ else: #process a csv of postal codes
         print(f"Cannot find csv file!")
     else:
         process_csv(csvFilePath)
+
+print(f"Quitting now...")
